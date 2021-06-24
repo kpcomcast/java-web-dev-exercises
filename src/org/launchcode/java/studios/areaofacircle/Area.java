f1635af3f5fe2417a3d30b3e344d19f0a861becd
Kiran Poulsen <kiran_poulsen@comcast.com>
2021-06-24 17:20:03 -0600

    found a solution for spaces and blanks

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.studios.areaofacircle;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Area {
     public static void main(String[] args) {
@@ -8,25 +9,39 @@ public class Area {
         double area;
         Scanner input;
         boolean valid = false;
+        Pattern p = Pattern.compile(".");
 
         input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
 
         while (!valid) {
-            if (!input.hasNextDouble()) {
-                System.out.println("Enter a valid double: ");
-                input.next();
+            String stuff = input.nextLine();
+            if (stuff.contains(" ") || stuff.isEmpty() || !stuff.matches("(.*)[0-9](.*)")) {
+                System.out.println("No blanks, spaces, or other characters. Only numbers.");
                 valid = false;
             } else {
-                radius = input.nextDouble();
-                if (radius < 0) {
-                    System.out.println("Enter a valid positive double: ");
-                    valid = false;
-                } else {
-                    valid = true;
-                }
+                System.out.println("Thanks for the number!");
+                radius = Double.valueOf(stuff);
+                valid = true;
             }
         }
+
+
+//        while (!valid) {
+//            if (!input.hasNextDouble() && input.hasNextLine()) {
+//                System.out.println("Enter a valid double: ");
+//                input.next();
+//                valid = false;
+//            } else {
+//                radius = input.nextDouble();
+//                if (radius < 0) {
+//                    System.out.println("Enter a valid positive double: ");
+//                    valid = false;
+//                } else {
+//                    valid = true;
+//                }
+//            }
+//        }
 
         input.close();
 
